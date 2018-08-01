d13be416ad0ceaab2b37263760daa7ef1f0046a9
Alex Perkins <acperkins@gmail.com>
2018-08-01 16:17:15 -0400

    Update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -43,7 +43,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function (params) {
   let user = this;
   let access = 'auth';
-  let token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  let token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
   // user.tokens.push({access, token});  // Doesn't work across all versions of mongodb
   user.tokens = user.tokens.concat([{access, token}]);
@@ -68,7 +68,7 @@ UserSchema.statics.findByToken = function (token) {
   let decoded;
 
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     // return new Promise((resolve, reject) => {
     //   reject();
